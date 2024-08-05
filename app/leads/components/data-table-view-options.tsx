9400f7d2814488b9f37701a313e3873c2d387d2e
abdvlla <abood67200@gmail.com>
2024-08-05 13:17:11 -0300

    commented out date range, will work on it later

--- app/leads/components/data-table-view-options.tsx
+++ app/leads/components/data-table-view-options.tsx
@@ -30,14 +30,10 @@ export function DataTableViewOptions<TData>({
           Add lead
         </Link>
       </Button>
-      <DatePickerWithRange />
+      {/* <DatePickerWithRange /> */}
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button
-            variant="outline"
-            size="sm"
-            className="hidden h-8 lg:flex ml-2"
-          >
+          <Button variant="outline" size="sm" className="hidden h-8 lg:flex">
             <MixerHorizontalIcon className="mr-2 h-4 w-4" />
             View
           </Button>
